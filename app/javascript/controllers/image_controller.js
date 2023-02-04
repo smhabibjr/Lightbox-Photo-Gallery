f6391cb2f606264d2f5f55f28a0ce809e30042ce
HabibJR <smhabib.abir2030@gmail.com>
2023-02-04 23:52:31 +0100

    add design for upload form

--- app/javascript/controllers/image_controller.js
+++ app/javascript/controllers/image_controller.js
@@ -3,8 +3,9 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="image"
 export default class extends Controller {
   connect() {
-    console.log("hi there");
-
+    $("#reload_slide_show").click(function (e){
+      location.reload();
+    })
 
 
   }
